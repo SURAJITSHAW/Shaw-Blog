2de92da3fab03de821200423f665a392b8ac7d76
Surajit <piklu3043@gmail.com>
2022-06-22 20:26:11 +0530

    learn: outputting list of data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
 const Home = () => {
-
-    // Variables which changes we want to show in our UI
-    const [name, setName] = useState("Riya Ghosh"); 
-
-    const clickHandle = () => {
-        // changes the value through the funct we get from useState, so our UI will reacts to the update
-        setName("Riya Shaw")
-    }
+    const [blogs, setBlogs] = useState([
+        { title: "My first blog", body: "lorem ipsum...", author: "Surajit Shaw", id: 1 },
+        { title: "My second blog", body: "lorem ipsum...", author: "Ayesha Shaw", id: 2 },
+        { title: "My third blog", body: "lorem ipsum...", author: "Riya Shaw", id: 3 },
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            
-            {/* Here we didn't invoke the funct, as we generally do in tradional HTML. Rather we just pass a refernce  to the funct. Otherwise it'll call itself automatically twice in the begining */}
-            <button onClick={clickHandle}>Click Me</button>
+            {/* Since we want to list through all elements in the blog array, and return or add a DOM node in the page. We'll using map() method and returns jsx format for each element */}
+
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
