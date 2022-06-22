c243ecb767d50cf71d09cbeb174891e9d5450b5b
Surajit <piklu3043@gmail.com>
2022-06-22 21:16:30 +0530

    add: added delete functionality

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,12 +1,12 @@
-const BlogList = ({blogArray: blogs, title}) => {
-
+const BlogList = ({ blogArray: blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
-        <h2>{ title }</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>written by {blog.author}</p>
+          <button onClick={ () => handleDelete(blog.id) }>Delete</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,24 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: "My first blog", body: "lorem ipsum...", author: "Surajit Shaw", id: 1 },
-        { title: "My second blog", body: "lorem ipsum...", author: "Ayesha Shaw", id: 2 },
-        { title: "My third blog", body: "lorem ipsum...", author: "Riya Shaw", id: 3 },
+        { title: "My first blog", body: "lorem ipsum...", author: "Riya", id: 1 },
+        { title: "My second blog", body: "lorem ipsum...", author: "Ayesha", id: 2 },
+        { title: "My third blog", body: "lorem ipsum...", author: "Riya", id: 3 },
+        { title: "My fourth blog", body: "lorem ipsum...", author: "Surajit", id: 4 },
+        { title: "My fifth blog", body: "lorem ipsum...", author: "Riya", id: 5 },
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
 
     return ( 
         <div className="home">
-            <BlogList title="All Blogs!" blogArray={ blogs }/>
+            {/* All Blogs */}
+            <BlogList title="All Blogs!" blogArray={ blogs } handleDelete={ handleDelete }/>
+            {/* Riya's Blogs */}
+            <BlogList title="Riya's Blogs!" blogArray={ blogs.filter((blog) => blog.author === "Riya") } handleDelete={ handleDelete }/>
         </div>
      );
 }
