d354b4bc85f56cb6eb343d71b055e7d346d3c331
Surajit <piklu3043@gmail.com>
2022-06-22 19:58:26 +0530

    add: useState(), onClick eventListeners

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    // Variables which changes we want to show in our UI
+    const [name, setName] = useState("Riya Ghosh"); 
+
+    const clickHandle = () => {
+        // changes the value through the funct we get from useState, so our UI will reacts to the update
+        setName("Riya Shaw")
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
+            
+            {/* Here we didn't invoke the funct, as we generally do in tradional HTML. Rather we just pass a refernce  to the funct. Otherwise it'll call itself automatically twice in the begining */}
+            <button onClick={clickHandle}>Click Me</button>
         </div>
      );
 }
