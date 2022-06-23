a04c2d3d086b094826d8f4f4aeb62e7246d75923
Surajit <piklu3043@gmail.com>
2022-06-23 19:05:22 +0530

    enhancement: blogs are opening to a new route

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom";
+import UseFetch from "../custom-hooks/useFetch";
 
 const BlogDetails = () => {
     // Getting the route parameter with the help of a built-in hook
     const { id } = useParams()
+    const { data: blogs, isPending, error } = UseFetch('http://localhost:8000/blogs/' + id)
     return ( 
         <div className="blog-details">
-            <h2>Blog - { id }</h2>
+            {/* Error message / screen */}
+            {error && <div>{ error }</div>}
+            {/* Loading screen */}
+            {isPending && <div>Loading...</div>}
+
+            { blogs && (
+                <article>
+                    <h2>{ blogs.title }</h2>
+                    <p>{ blogs.author }</p>
+                    <div>{ blogs.body }</div>
+                </article>
+            )}
         </div>
      );
 }
