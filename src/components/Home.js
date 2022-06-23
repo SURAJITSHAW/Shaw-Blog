579272baa86204339ac4b6c07d7dfc7f1a141c7c
Surajit <piklu3043@gmail.com>
2022-06-23 09:58:47 +0530

    enhancement: added error message for unauthorized end points, promise get rejected

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,26 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     // Calling the useEffect hook only once to fetch the data
     useEffect(() => {
       
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok) {
+                    throw Error("Can't fetch the data. Server is down :(")
+                }
+
+                setError(null)
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
+                setIsPending(false)
+            })
+            .catch(err => {
+                setError(err.message)
                 setIsPending(false)
             })
 
@@ -21,6 +31,8 @@ const Home = () => {
     
     return ( 
         <div className="home">
+            {/* Error message / screen */}
+            {error && <div>{ error }</div>}
             {/* Loading screen */}
             {isPending && <div>Loading...</div>}
             {/* All Blogs */}
