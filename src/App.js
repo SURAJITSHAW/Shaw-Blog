c75082206ca372dd8776c2b9366ad900a32ee14c
Surajit <piklu3043@gmail.com>
2022-06-24 13:36:04 +0530

    added hashrouter in index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Create from "./components/Create";
 import BlogDetails from "./components/BlogDetails";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <Router>
+
       <div className="App">
         <Navbar />
         <div className="content">
@@ -27,7 +27,7 @@ function App() {
           </Switch>
         </div>
       </div>
-    </Router>
+
   );
 }
 
